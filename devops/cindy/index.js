9ebc5baddd199b0832da106788cf6bcac8630d8b
Dvdam <rdvdam@gmail.com>
2019-09-15 15:59:46 -0300

    David Ejercicio

--- devops/cindy/index.js
+++ devops/cindy/index.js
@@ -1,10 +1,9 @@
-const getResponseObject = () => {
+const getResponseObject = () =>
         ({
                 "1a": "This is an exercise.",
-                1b: "You must fix the error",
+                "1b": "You must fix the error",
                 "1c": "and make a PR."
         });
-};
 
 const main = () => Object.values(getResponseObject()).join(" ");
 
